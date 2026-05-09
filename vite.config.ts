da14608b9b3ad336f6c9653dbed902c4c13ae522
FelipeOliverb <felipe235711@gmail.com>
2026-05-09 13:59:13 -0300

    Instala react-is

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  base: '/APS-2026-1/',
+  plugins: [react()],
+  server: {
+    port: 3000
+  }
 })
